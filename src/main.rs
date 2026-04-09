414347fc9b661e59a2365aa6f218e2c6f29fad81
兰陵王 <gemiman@vip.qq.com>
2026-04-09 15:00:21 +0800

    feat: 添加 Axum Web 服务器框架

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, debug_handler, routing};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", routing::get(index));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on http://0.0.0.0:3000");
+    axum::serve(listener, router).await.unwrap();
+}
+
+#[debug_handler]
+async fn index() -> &'static str {
+    "Hello Rust!"
 }
