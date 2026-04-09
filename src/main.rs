e42cdf8644cb5fee7ef570496790ab8aa7238cc9
兰陵王 <gemiman@vip.qq.com>
2026-04-09 15:22:11 +0800

    feat: 添加日志模块

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+pub mod logger;
+
 use axum::{Router, debug_handler, routing};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    logger::init();
     let router = Router::new().route("/", routing::get(index));
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("listening on http://0.0.0.0:3000");
+    tracing::info!("listening on http://0.0.0.0:3000");
     axum::serve(listener, router).await.unwrap();
 }
 
