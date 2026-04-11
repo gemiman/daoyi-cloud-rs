363d9cd0d5858f5023fd27021f34df47f61c02d9
兰陵王 <gemiman@vip.qq.com>
2026-04-11 22:34:55 +0800

    refactor: 简化路由创建逻辑

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,13 +3,7 @@ use daoyi_cloud_common::app::AppState;
 use daoyi_module_demo::demo;
 
 pub fn create_router() -> Router<AppState> {
-    Router::new().nest(
-        "/admin-api",
-        Router::new().nest(
-            "/demo",
-            Router::new().nest("/users", demo::admin_api::user::create_router()),
-        ),
-    )
+    Router::new().merge(demo::create_router())
 }
 
 // impl IntoResponse for anyhow::Error {
